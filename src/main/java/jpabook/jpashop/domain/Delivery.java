12d8ff928d21ce88c413556f3ec4c4fd20f75459
isemang <a55306039@gmail.com>
2021-12-20 23:59:04 +0900

    feat - 실전예제 3: 다양한 연관관계 매핑에서 1:N, 1:1, M:N 매핑 예시 코드 추가

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+    private DeliveryStatus status;
+
+    @OneToMany(mappedBy = "delivery")
+    private Order order;
+}
